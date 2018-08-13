ff0456e2864118362900ea78e6dfab4711d7491f
scorpidio11 <scorpido@gmail.com>
2018-08-12 17:03:54 -0700

    final

---
+++ assets/javascript/game.js
@@ -0,0 +1,62 @@
+
+
+    var wins = 0;
+    var loses = 0;
+    var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    //var computerChoices = "abcdefghijklmnopqrstuvwxyz";
+    var guessLeft = 10;
+
+    var userGuessTotal = [];
+    var computerGuessTotal = [];
+
+    var reset = function () {
+        document.location.reload(true);
+
+    }
+
+
+
+
+    document.onkeyup = function (event) {
+        var userGuess = event.key.toLowerCase();
+        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        userGuessTotal.push(userGuess);
+        computerGuessTotal.push(computerGuess);
+
+        if (guessLeft === 0) {
+
+            reset();
+            alert("Let\'s try again.");
+
+            //document.getElementsByID('game').innerHTML.remove();
+        }
+
+        if ((userGuess === computerGuess)) {
+            wins++;
+        }
+
+        if ((userGuess === computerGuess) || userGuess !== computerGuess) {
+            guessLeft--;
+        }
+
+
+        else ((userGuess !== computerGuess))
+        {
+            loses++;
+        }
+
+
+
+        // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
+        var html =
+
+
+            "<p>Wins: " + wins + "</p>" +
+            "<p>Losses: " + loses + "</p>" +
+            "<p>Guesses Left: " + guessLeft + "</p>" +
+            "<p>Computer Guesses so far:  " + computerGuessTotal + "</p>" +
+            "<p>Your Guesses so far: " + userGuessTotal + "</p>";
+
+        // Set the inner HTML contents of the #game div to our html string
+        document.querySelector("#game").innerHTML = html;
+    };
