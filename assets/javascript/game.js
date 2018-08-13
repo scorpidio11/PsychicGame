ce5ad6d07fcfa13e4f45d5a0c91ae2a9b906946d
scorpidio11 <scorpido@gmail.com>
2018-08-13 16:51:35 -0700

    modified

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,51 @@
 
-
+    // VARIABLES
+      // ==========================================================================
     var wins = 0;
     var loses = 0;
     var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     //var computerChoices = "abcdefghijklmnopqrstuvwxyz";
-    var guessLeft = 10;
+    var guessLeft = 9;
 
     var userGuessTotal = [];
-    var computerGuessTotal = [];
+    //var computerGuessTotal = [];
 
-    var reset = function () {
-        document.location.reload(true);
+    // var reset = function () {
+    //     document.location.reload(true);
 
-    }
+    // }
 
-       //When User Push Keyboard
-    document.onkeyup = function () {
+     // FUNCTIONS
+      // ==============================================================================
+    document.onkeyup = function (event) {
         var userGuess = event.key.toLowerCase();
         var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
     
       //Guessed Alpahet added to empty arry and show on screen
         userGuessTotal.push(userGuess);
-        computerGuessTotal.push(computerGuess);
+      
 
-        if (guessLeft === 0) {
-
-            reset();
-            alert("Let\'s try again.");
-
-            //document.getElementsByID('game').innerHTML.remove();
-        }
-
-        if ((userGuess === computerGuess)) {
+        if (userGuess == computerGuess){
             wins++;
+            guessLeft = 9;
+            userGuessTotal.length = 0;
         }
 
-        if ((userGuess === computerGuess) || userGuess !== computerGuess) {
-            guessLeft--;
-        }
-
-
-        else ((userGuess !== computerGuess))
+        else if  (guessLeft == 0)
+        
         {
             loses++;
+            guessLeft = 9;
+            userGuessTotal.length = 0;
+          
         }
-  
+
+        else if (userGuess !== computerGuess){
+            guessLeft--; //decrementing the guesses left
+        }  
+
+      
+
 
         // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
         var html =
@@ -53,7 +54,7 @@
             "<p>Wins: " + wins + "</p>" +
             "<p>Losses: " + loses + "</p>" +
             "<p>Guesses Left: " + guessLeft + "</p>" +
-            "<p>Computer Guesses so far:  " + computerGuessTotal + "</p>" +
+            // "<p>Computer Guesses so far:  " + computerGuessTotal + "</p>" +
             "<p>Your Guesses so far: " + userGuessTotal + "</p>";
 
         // Set the inner HTML contents of the #game div to our html string
